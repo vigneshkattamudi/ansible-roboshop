2108b6c6eec8d04119740a2a9be19c7c0646aedb
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-09 08:31:21 +0000

    fix: add ansible-roles as normal folder

---
+++ ansible.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ----------- CONFIGURABLE INPUTS -------------
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0f63039a616382c25"          # e.g. sg-0123456789abcdef
+# ----------------------------------------------
+
+# ---------- CLOUD-INIT USER DATA -------------
+USER_DATA=$(cat << 'EOF'
+#cloud-config
+packages:
+  - ansible
+runcmd:
+  - dnf install -y ansible
+EOF
+)
+# ----------------------------------------------
+
+echo "Launching EC2 instance..."
+
+aws ec2 run-instances \
+  --image-id "$AMI_ID" \
+  --instance-type t3.micro \
+  --security-group-ids "$SG_ID" \
+  --no-key-pair \
+  --user-data "$USER_DATA" \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Ansible-Host}]'
