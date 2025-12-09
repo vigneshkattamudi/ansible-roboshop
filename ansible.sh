9ebc93ede34dc3ef69783af4fbcc49237a536c73
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-09 08:40:17 +0000

    roboshop

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,7 @@
 
 # ----------- CONFIGURABLE INPUTS -------------
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0f63039a616382c25"          # e.g. sg-0123456789abcdef
+SG_ID="sg-0f63039a616382c25"          
 # ----------------------------------------------
 
 # ---------- CLOUD-INIT USER DATA -------------
@@ -22,6 +22,5 @@ aws ec2 run-instances \
   --image-id "$AMI_ID" \
   --instance-type t3.micro \
   --security-group-ids "$SG_ID" \
-  --no-key-pair \
   --user-data "$USER_DATA" \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Ansible-Host}]'
